0b9af7184584843efc4191af597ada25c857022f
ciazhar <ciazhar.id@gmail.com>
2017-11-16 23:47:00 +0700

    fix consume api register, save, findall workshop participant

--- js/angular/workshop-participant.js
+++ js/angular/workshop-participant.js
@@ -4,7 +4,7 @@ app.controller('workshopController',function ($scope, $http, $window) {
     $scope.daftarParticipant = {};
 
     $scope.listParticipant = function(){
-        $http.get('http://localhost:8080/workshop/all').then(sukses,gagal);
+        $http.get('/workshop/all').then(sukses,gagal);
         function sukses(response) {
             $scope.daftarParticipant = response.data;
             console.log('Response : '+response);
@@ -15,13 +15,31 @@ app.controller('workshopController',function ($scope, $http, $window) {
     };
     $scope.listParticipant();
 
-    $scope.saveWorkshopParticipant = function(){
-        $http.post('/http://localhost:8080/workshop/save',$scope.participant).then(sukses,gagal);
+    $scope.registerWorkshopParticipant = function(){
+        $http.post('/workshop/register',$scope.participant).then(sukses,gagal);
         function sukses(response){
-            $scope.listParticipant();
+            $window.location.href = '/workshop-participant.html';
+            console.log($scope.participant.toString())
         }
         function gagal(response){
+            console.log('Data: ' +$scope.participant.toString())
             console.log('Error : '+response)
         }
+    }
+    $scope.updateWorkshopParticipant = function(){
+        $http.post('/workshop/update',$scope.participant).then(sukses,gagal);
+        function sukses(response){
+            $window.location.href = '/workshop-participant.html';
+            console.log($scope.participant)
+        }
+        function gagal(response){
+            console.log('Data: '+$scope.participant)
+            console.log('Error : '+response)
+        }
+    }
+
+    $scope.selectParticipant = function(participant){
+        $scope.participant = participant
+        console.log(participant)
     }
 })
